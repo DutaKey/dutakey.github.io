eb8b7f5501ce8548816b29d10932fadd809df305
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2023-08-10 14:29:57 +0700

    Initial commit

---
+++ gitprofile.config.js
@@ -0,0 +1,174 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'dutakey', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 10, // How many projects to display.
+    exclude: {
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: 'dutapashafebrian',
+    twitter: '',
+    mastodon: '',
+    facebook: '',
+    instagram: 'duta_fbriann',
+    youtube: '', // example: 'pewdiepie'
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: 'DutaPashaFebrian',
+    website: 'https://dutakey.github.io',
+    phone: '',
+    email: 'dutapashafebrian@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'PHP',
+    'Laravel',
+    'JavaScript',
+    'React.js',
+    'Node.js',
+    'MySQL',
+    'PostgreSQL',
+    'Git',
+    'Docker',
+    'CSS',
+    'Antd',
+    'Bootstrap',
+    'Tailwind',
+  ],
+  experiences: [
+    {
+      company: 'Djempol Group',
+      position: 'Programmer',
+      from: 'September 2021',
+      to: 'Present',
+      companyLink: 'http://djempol.id/',
+    },
+  ],
+  /* certifications: [
+    {
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com'
+    },
+  ], */
+  education: [
+    {
+      institution: 'SMK Negri 7 Pontianak',
+      degree: 'Siswa',
+      from: '2021',
+      to: '2024',
+    },
+    {
+      institution: 'MTS Al Irsyad',
+      degree: 'Passed',
+      from: '2019',
+      to: '2021',
+    },
+  ],
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [
+    {
+      title: 'SnapApp',
+      description:
+        'WebSite Downloader For Instagram, Tiktok, Youtube, and Facebook',
+      imageUrl: 'https://snapapp.my.id/img/logo-snap.png',
+      link: 'https://snapapp.my.id/',
+    }
+  ],
+  // Display blog posts from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 2, // How many posts to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'wireframe',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+    
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'halloween',
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://github.com/dutakey"
+      target="_blank"
+      rel="noreferrer"
+    >DutaKey</a> and ❤️`,
+};
+
+export default config;
